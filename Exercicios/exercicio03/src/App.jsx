955c98cb89cef1d59bba63d4df9c46ca8e1d18df
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-08-20 12:32:30 -0300

    Exercicio 3
    Exercicio 3 quase pronto, faltando algumas centralizações e adição de blocos. Neste commit, fiz o 80% do bloco central, sendo, o quadrado vermelho e os 4 quadradinhos veremelho fraco

--- Exercicios/exercicio03/src/App.jsx
+++ Exercicios/exercicio03/src/App.jsx
@@ -1,6 +1,7 @@
 import BarraVermelha from "./componentes/BarraVermelha";
 import BarraVermelhoFraco from "./componentes/BarraVermelhoFraco";
 import QuadradoVermelho from "./componentes/QuadradoVermelho";
+import QuadradoVermelhoFraco from "./componentes/QuadradoVermelhoFraco";
 import "./App.css";
 
 function App() {
@@ -10,8 +11,23 @@ function App() {
       <BarraVermelhoFraco idBarra={"barraVermelhoFraco"}></BarraVermelhoFraco>
       <div id="main">
         <QuadradoVermelho idBloco={"quadradoVermelho"}></QuadradoVermelho>
-        <div id="quatroBlocos">
-
+        <div id="quatroBloco">
+          <QuadradoVermelhoFraco
+            classBloco={"quadradoVermelhoFraco"}
+            conteudo={1}
+          ></QuadradoVermelhoFraco>
+          <QuadradoVermelhoFraco
+            classBloco={"quadradoVermelhoFraco"}
+            conteudo={2}
+          ></QuadradoVermelhoFraco>
+          <QuadradoVermelhoFraco
+            classBloco={"quadradoVermelhoFraco"}
+            conteudo={3}
+          ></QuadradoVermelhoFraco>
+          <QuadradoVermelhoFraco
+            classBloco={"quadradoVermelhoFraco"}
+            conteudo={4}
+          ></QuadradoVermelhoFraco>
         </div>
       </div>
     </>
---
+++ Exercicios/exercicio03/src/componentes/QuadradoVermelhoFraco.jsx
@@ -0,0 +1,9 @@
+function QuadradoVermelhoFraco({ classBloco, conteudo }) {
+  return (
+    <>
+      <div className={classBloco}>{conteudo}</div>
+    </>
+  );
+}
+
+export default QuadradoVermelhoFraco;
